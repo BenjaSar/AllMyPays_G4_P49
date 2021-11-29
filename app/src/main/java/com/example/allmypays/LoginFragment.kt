b8aab0ef43ff9963e0d45497957d373f268e61af
BenjaSar <fsarmiento1805@gmail.com>
2021-11-28 23:46:53 -0300

    Se agrego interactividad al login. Login y pagina de prueba funcionando

---
+++ app/src/main/java/com/example/allmypays/LoginFragment.kt
@@ -0,0 +1,48 @@
+package com.example.allmypays
+
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.android.synthetic.main.login_fragment.view.*
+
+/**
+ * Fragment representing the login screen for AllMyPays.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+        //Evento next_button para validar la contraseña
+        view.login.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password.error = getString(R.string.error_password)
+            } else {
+                //Clear the error
+                password.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text!!)) {
+                // Clear the error.
+                password.error = null
+            }
+            false
+        }
+        return view
+    }
+}
+
+// "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+private fun isPasswordValid(text:Editable?): Boolean{
+    return text != null && text.length>=8
+}
---
+++ app/src/main/java/com/example/allmypays/ProductGridFragment.kt
@@ -0,0 +1,17 @@
+package com.example.allmypays
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class ProductGridFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.product_grid_fragment, container, false)
+    }
+}
